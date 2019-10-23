a0a8592987cfb6e4c24259de4af5e49fd2e33984
w.zdorowa <w.zdorowa@gmail.com>
2019-10-23 19:20:31 +0300

    Начала описывать базовую структуру модели

--- src/slider/model.js
+++ src/slider/model.js
@@ -1,3 +1,49 @@
 class Model {
-    
+    constructor () {
+        this.initialConfig {
+            min = 0;
+            max =  100;
+            state = [10, 50];
+            horizontal = true;
+            vertical = false;
+            amount = 2;
+            step = 1;
+            tultip = true;
+        };
+        this.currentConfig {};
+    }
+    setMinValue(value) {
+        this.currentConfig.min = value;
+    }
+    setMaxValue(value) {
+        this.currentConfig.max = value;
+    }
+    setStateValue(arr) {
+        this.currentConfig.state[0] = arr[0];
+        this.currentConfig.state[1] = arr[1];
+        // и т.д. (написать нормальную реализацию через цикл)
+    }
+    // написать нормальные значения для сравнения в условных конструкциях
+    setOrientationValue(value) {
+        if(value === horizontal) {
+            this.currentConfig.horizontal = true;
+            this.currentConfig.vertical = false;
+        } else if(value === vertical) {
+            this.currentConfig.horizontal = false;
+            this.currentConfig.vertical = true;
+        }
+    }
+    setAmountValue(value) {
+        this.currentConfig.amount = value;
+    }
+    setStepValue(value) {
+        this.currentConfig.step = value;
+    }
+    setTulipValue(value) {
+        if(value === 'true') {
+            this.currentConfig.tultip = true;
+        } else if(value === 'false') {
+            this.currentConfig.tultip = false;
+        }
+    }
 }
