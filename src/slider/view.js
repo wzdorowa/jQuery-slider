0843d95cafbe7bf5c7e7cc28516a94b4793ad347
w.zdorowa <w.zdorowa@gmail.com>
2019-10-26 16:35:55 +0300

    Делаю первую попытку придумать вью

--- src/slider/view.js
+++ src/slider/view.js
@@ -1,3 +1,29 @@
 class View {
-    
+    sliderTouch = document.createElement('div');
+    sliderTouch.className = "slider-touch";
+    sliderTouch.innerHTML = "<span></span>";
+
+    sliderLine = document.createElement('div');
+    sliderLine.className = "slider-line";
+    sliderLine.innerHTML = "<span></span>";
+
+    slider = document.getElementById('my-slider');
+
+    /* каким-то образом получить данные о количестве ползунков слайдера */
+
+    /* тут должна быть функция, которая будет создавать необходимое
+    количество тачей основываясь на полученных данных о их количестве, что-то типа такой: 
+    function getListContent() {
+        let result = [];
+
+        for(let i=1; i<=3; i++) {
+            let li = document.createElement('li');
+            li.append(i);
+            result.push(li);
+        }
+
+        return result;
+        }
+*/
+    document.slider.append(sliderTouch);
 }
