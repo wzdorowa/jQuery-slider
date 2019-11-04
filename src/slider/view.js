e37f82a96b3da3c0cd1c46f91d5e37c164e15b85
w.zdorowa <w.zdorowa@gmail.com>
2019-11-04 13:43:03 +0300

    написала примерную реализацию функции reset которая устанавливает/сбрасывает настройки расположения ползунков по-умолчанию

--- src/slider/view.js
+++ src/slider/view.js
@@ -10,17 +10,34 @@ class View {
         this.sliderLine.innerHTML = "<span></span>";
 
         this.slider = id;
+
+        this.normolizeFact = sliderTouch.offsetWidth;
     }
 
     /* каким-то образом получить данные о количестве ползунков слайдера 
     из модели amount = currentConfig.amount*/
 
     /* функция, которая будет создавать необходимое количество ползунков основываясь 
-    на полученных данных о их количестве, что-то типа такой: */
+        на полученных данных о их количестве, что-то типа такой: */
     createSlider(slider) {
         for(let i = 1; i <= amount; i++) {
             document.slider.append(sliderTouch);
         }
         document.slider.append(sliderLine);
     }
+
+    /* функция reset устанавливает/сбрасывает настройки расположения ползунков 
+    на начальные настройки по умолчанию.
+    -найти все кнопки-ползунки внутри слайдера */ 
+    reset(slider) {
+        let elements = slider.querySelector(".slider-touch");
+        let sumNormlizeFact = 0;
+        for(let i = 0; i <= elements.length; i++) {
+            elements[i].style.left = this.normolizeFact + sumNormlizeFact + 'px';
+            sumNormlizeFact + this.normolizeFact;
+        }
+        sliderLine.style.marginLeft = '0px';
+        sliderLine.style.width = (slider.offsetWidth - (elements[elements.length - 1].offsetWidth)) + 'px';
+    }
+
 }
