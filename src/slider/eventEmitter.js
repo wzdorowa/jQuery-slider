1239b5164a04a627c24113e1ac3eb8ddc1370b7f
w.zdorowa <w.zdorowa@gmail.com>
2019-11-18 13:26:09 +0300

    создала класс eventEmitter, написала для него методы emit и subscribe

---
+++ src/slider/eventEmitter.js
@@ -0,0 +1,23 @@
+export class EventEmitter {
+    constructor() {
+        this.handlersByEvent = {};
+    }
+    emit(eventName, data) {
+        const handlers = this.handlersByEvent[eventName];
+        if(handlers) {
+            handlers.forEach(fn => {
+                fn.call(null, data);
+            });
+        }
+    }
+    subscribe(eventName, fn) {
+        if(!this.handlersByEvent[eventName]) {
+            this.handlersByEvent[eventName] = [];
+        }
+
+        this.handlersByEvent[eventName].push(fn);
+        return () => {
+            this.handlersByEvent[eventName] = this.handlersByEvent[eventName].filter(eventFn => fn !== eventFn);
+        }
+    }
+}
