a908ebbd55ee2101e608454eea757ac50d151ab0
w.zdorowa <w.zdorowa@gmail.com>
2019-11-28 11:16:36 +0300

    Изменила текущее значение шага в стейте модели

--- src/slider/model.js
+++ src/slider/model.js
@@ -9,7 +9,7 @@ export class Model {
             horizontal: true,
             vertical: false,
             amount: 4,
-            step: 1,
+            step: 2,
             tooltip: true,
         };
         this.currentConfig = {};
