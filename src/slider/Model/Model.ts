7cd556f2e232f86c39c79a2ca6ab8372117fd791
w.zdorowa <w.zdorowa@gmail.com>
2022-03-25 10:48:06 +0300

    Сделала из normalizeState чистую функцию, которая возвращает объект нового валидного стэйта. #42

--- src/slider/Model/Model.ts
+++ src/slider/Model/Model.ts
@@ -22,8 +22,8 @@ class Model {
   }
 
   public updateState(state: IModelState): void {
-    this.state = state;
-    this.normalizeState();
+    this.state = this.normalizeState(state);
+    this.notifyStateChanged();
   }
 
   public requestThumbValueChange(value: number, index: number): void {
@@ -57,93 +57,96 @@ class Model {
   public setNewThumbValue(thumbValue: number, index: number): void {
     this.state.thumbsValues[index] = thumbValue;
 
-    this.checkThumbsValuesIntersection(index);
+    this.state.thumbsValues = this.checkThumbsValuesIntersection(
+      index,
+      this.state,
+    );
     this.notifyThumbsValuesChanged();
   }
 
-  private normalizeState() {
-    if (this.state.step <= 0) {
-      this.state.step = 1;
+  private normalizeState(state: IModelState): IModelState {
+    const newState = state;
+    if (newState.step <= 0) {
+      newState.step = 1;
     }
 
-    if (this.state.thumbsValues.length === 0) {
-      this.state.thumbsValues[0] = this.state.min;
+    if (newState.thumbsValues.length === 0) {
+      newState.thumbsValues[0] = newState.min;
     }
 
-    if (!['horizontal', 'vertical'].includes(this.state.orientation)) {
-      this.state.orientation = defaultState.orientation;
+    if (!['horizontal', 'vertical'].includes(newState.orientation)) {
+      newState.orientation = defaultState.orientation;
     }
 
-    if (!Number.isInteger(this.state.min)) {
-      this.state.min = Math.floor(this.state.min);
+    if (!Number.isInteger(newState.min)) {
+      newState.min = Math.floor(newState.min);
     }
 
     if (!Number.isInteger(this.state.max)) {
-      this.state.max = Math.floor(this.state.max);
+      newState.max = Math.floor(newState.max);
     }
 
     const minPossibleMaxValue =
-      this.state.min + this.state.step * this.state.thumbsValues.length;
+      newState.min + newState.step * newState.thumbsValues.length;
 
-    if (this.state.max < minPossibleMaxValue) {
-      this.state.max = minPossibleMaxValue;
+    if (newState.max < minPossibleMaxValue) {
+      newState.max = minPossibleMaxValue;
     }
 
-    this.checkThumbsValuesIntersection(null);
-    this.notifyStateChanged();
+    newState.thumbsValues = this.checkThumbsValuesIntersection(null, newState);
+
+    return newState;
   }
 
-  private checkThumbsValuesIntersection(thumbIndex: number | null): void {
+  private checkThumbsValuesIntersection(
+    thumbIndex: number | null,
+    state: IModelState,
+  ): number[] {
     let index = thumbIndex;
     if (index === null) {
       index = 0;
     }
-    for (let i = index; i < this.state.thumbsValues.length; i += 1) {
-      this.normalizeThumbValue(this.state.thumbsValues[i], i);
-      if (this.state.thumbsValues[i] > this.state.thumbsValues[i + 1]) {
-        this.state.thumbsValues[i + 1] = this.state.thumbsValues[i];
+    const thumbValues = state.thumbsValues;
+    for (let i = index; i < thumbValues.length; i += 1) {
+      thumbValues[i] = this.normalizeThumbValue(thumbValues[i], state);
+      if (thumbValues[i] > thumbValues[i + 1]) {
+        thumbValues[i + 1] = thumbValues[i];
       }
     }
 
     for (let i = index; i > 0; i -= 1) {
-      this.normalizeThumbValue(this.state.thumbsValues[i], i);
-      if (this.state.thumbsValues[i] < this.state.thumbsValues[i - 1]) {
-        this.state.thumbsValues[i - 1] = this.state.thumbsValues[i];
+      thumbValues[i] = this.normalizeThumbValue(thumbValues[i], state);
+      if (thumbValues[i] < thumbValues[i - 1]) {
+        thumbValues[i - 1] = thumbValues[i];
       }
     }
+
+    return thumbValues;
   }
 
-  private normalizeThumbValue(thumbsValue: number, index: number): void {
-    const lastStep =
-      Math.round(((this.state.max - this.state.min) % this.state.step) * 10) /
-      10;
+  private normalizeThumbValue(thumbsValue: number, state: IModelState): number {
+    const { min, max, step } = state;
+    const lastStep = Math.round(((max - min) % step) * 10) / 10;
 
-    const previousLastStep = this.state.max - lastStep;
+    const previousLastStep = max - lastStep;
 
     let value: number = Math.round(thumbsValue * 100) / 100;
 
     if (lastStep > 0 && value > previousLastStep + lastStep / 2) {
-      value =
-        Math.round((value - this.state.min) / this.state.step) *
-          this.state.step +
-        this.state.min +
-        lastStep;
+      value = Math.round((value - min) / step) * step + min + lastStep;
     } else {
-      value =
-        Math.round((value - this.state.min) / this.state.step) *
-          this.state.step +
-        this.state.min;
+      value = Math.round((value - min) / step) * step + min;
     }
 
     value = Math.round(value * 100) / 100;
 
-    if (value < this.state.min) {
-      value = this.state.min;
-    } else if (value >= this.state.max) {
-      value = this.state.max;
+    if (value < min) {
+      value = min;
+    } else if (value >= max) {
+      value = max;
     }
 
-    this.state.thumbsValues[index] = value;
+    return value;
   }
 
   private notifyStateChanged(): void {
